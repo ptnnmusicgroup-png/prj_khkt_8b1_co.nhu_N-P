e894b90acfa7ffa35354a85c8f565ea605038fe6
Phuong Me Dev (Part Of PTNN-Dev Grp) <ptnn.musicgroup@gmail.com>
2026-09-15 22:48:29 +0700

    Delete admin-config.js

--- admin-config.js
+++
@@ -1,3 +0,0 @@
-// Replace this placeholder with the Google email of the KatLearn administrator.
-// Example: window.KATLEARN_ADMIN_EMAILS = ['yourname@gmail.com'];
-window.KATLEARN_ADMIN_EMAILS = [];
