db2457d32fdc3407adb6074a8a43ef7b21710073
Phuong Me Dev (Part Of PTNN-Dev Grp) <ptnn.musicgroup@gmail.com>
2026-09-15 22:48:50 +0700

    Add files via upload

---
+++ admin-config.js
@@ -0,0 +1,3 @@
+// Replace this placeholder with the Google email of the KatLearn administrator.
+// Example: window.KATLEARN_ADMIN_EMAILS = ['yourname@gmail.com'];
+window.KATLEARN_ADMIN_EMAILS = ['katlearn.admin@gmail.com'];
